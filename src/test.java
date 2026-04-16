e379027278d27efe53d355cdf78adae95924d9e5
why035 <13313914658@163.com>
2026-04-17 00:46:05 +0800

    1

---
+++ src/test.java
@@ -0,0 +1,17 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		int n = 3;
+		while (n != 1) {
+		    System.out.println(n);
+		    if (n % 2 == 0) {
+		        n = n / 2;
+		    } else {
+		        n = 3 * n + 1;
+		    }
+		}
+		System.out.println(n);
+	}
+
+}
